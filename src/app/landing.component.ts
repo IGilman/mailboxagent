31890622b6ad43321ba54e58735548cd9c2535c6
Isaac Gilman <gilman.isaac@gmail.com>
2025-08-28 21:25:13 -0400

    fixing responsiveness and cleaning up unused css

--- src/app/landing.component.ts
+++ src/app/landing.component.ts
@@ -3,14 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
     MatButtonModule,
     MatIconModule,
     MatDividerModule
@@ -19,25 +17,8 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
-  formData = {
-    name: '',
-    email: '',
-    message: ''
-  };
-  
-  // Method to get the current year for the copyright notice
+
   getCurrentYear(): number {
     return new Date().getFullYear();
-  }
-
-  submitForm() {
-    console.log('Form submitted:', this.formData);
-    // Here you would normally send the form data to your backend
-    alert('Thank you for your message! We\'ll get back to you soon.');
-    this.formData = {
-      name: '',
-      email: '',
-      message: ''
-    };
   }
 }
